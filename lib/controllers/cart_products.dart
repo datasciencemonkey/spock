152c7a43273c317aeb006d673abf6e531b7d4d5d
Sathish Gangichetty <datasciencemonkey@gmail.com>
2020-09-15 00:10:46 -0400

    add comments on controller

--- lib/controllers/cart_products.dart
+++ lib/controllers/cart_products.dart
@@ -5,15 +5,21 @@ class UserCartProductsController extends GetxController {
   List<Content> _items = [];
   String _user;
 
+// getter for items
   List<Content> get items {
     return [..._items];
   }
 
+// getter for user
+  String get user => _user;
+
+// add product to cart
   void addProduct(Content item) {
     _items.add(item);
     update();
   }
 
+// update user post login
   void updateUser(String user) {
     _user = user;
     update();
